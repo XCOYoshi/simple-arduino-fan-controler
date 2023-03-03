0549462036549341286aebb111dc210921e8d91b
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2023-03-03 22:04:55 +0100

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,14 +1,9 @@
-#include <GyverGFX.h> //lib https://github.com/GyverLibs/GyverGFX
 #include <GyverMAX6675.h> // lib https://github.com/GyverLibs/GyverMAX6675
 
-//setings
-const int startfanat = 40;
-const int stopfanat = 28;
-
 //pinaut interface to MAX6675
-#define CLK_PIN 8 //sck
-#define DATA_PIN  7 //S0
-#define CS_PIN  6 //CS
+#define CLK_PIN 13 //sck
+#define DATA_PIN  12 //S0
+#define CS_PIN  10 //CS
 
 GyverMAX6675<CLK_PIN, DATA_PIN, CS_PIN> sens;  // sens.readTemp to read sensore data sens.getTemp()is to get the temprature sens.getTempInt() to get the remp as init
 
@@ -18,11 +13,12 @@ const int relais2 = 3; // low is on
 const int relais3 = 4; // low is on
 const int relais4 = 5; // low is on
 
-//aux outputs
-const int onboardled = 13;
-
 //system storage
 bool runfan1 = false;
+
+//setings
+const float startfanat = 35.00;
+const float stopfanat = 30.00;
 
 void setup() {
   Serial.begin(9600);
@@ -32,27 +28,28 @@ void setup() {
   pinMode(relais2, OUTPUT);
   pinMode(relais3, OUTPUT);
   pinMode(relais4, OUTPUT);
-  pinMode(onboardled, OUTPUT);
 
 }
 
 void loop() {
-  Serial.print(runfan1);
-  tempsenseandrunfan1(relais1, sens.getTempInt(), startfanat, stopfanat);
 
+ if (sens.readTemp()){
+   tempsenseandrunfan1(relais1, sens.getTemp(), startfanat, stopfanat);
+   Serial.print(sens.getTemp());
+   Serial.print("\n");
+  };
+ delay(1000);
   
 }
 
-void tempsenseandrunfan1(int relai, int temprature, int startfantmp, int stopfantmp){
+void tempsenseandrunfan1(int relai, float temprature, int startfantmp, int stopfantmp){
   // temp sense
   if(temprature >= startfantmp){runfan1 = true;}
   if(temprature <= stopfantmp){runfan1 = false;}
 
   // run fan or turn of fan
   if(runfan1 == true){ 
-                      digitalWrite(relai, LOW);
-                      digitalWrite(onboardled, LOW);}
+                      digitalWrite(relai, LOW);}
   if(runfan1 == false){
-                        digitalWrite(relai, HIGH); 
-                        digitalWrite(onboardled, HIGH);}
+                        digitalWrite(relai, HIGH);} 
   }
