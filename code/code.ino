6e257c977af051bae3e757497efb623e6d43d66f
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2023-03-03 12:18:23 +0100

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -18,6 +18,9 @@ const int relais2 = 3; // low is on
 const int relais3 = 4; // low is on
 const int relais4 = 5; // low is on
 
+//aux outputs
+const int onboardled = 13;
+
 //system storage
 bool runfan1 = false;
 
@@ -29,11 +32,12 @@ void setup() {
   pinMode(relais2, OUTPUT);
   pinMode(relais3, OUTPUT);
   pinMode(relais4, OUTPUT);
+  pinMode(onboardled, OUTPUT);
 
 }
 
 void loop() {
-  Serial.print(sens.getTempInt());
+  Serial.print(runfan1);
   tempsenseandrunfan1(relais1, sens.getTempInt(), startfanat, stopfanat);
 
   
@@ -42,9 +46,13 @@ void loop() {
 void tempsenseandrunfan1(int relai, int temprature, int startfantmp, int stopfantmp){
   // temp sense
   if(temprature >= startfantmp){runfan1 = true;}
-  if(temprature <= stopfantmp){runfan1 = true;}
+  if(temprature <= stopfantmp){runfan1 = false;}
 
   // run fan or turn of fan
-  if(runfan1 == true){digitalWrite(relai, LOW);}
-  if(runfan1 == false){digitalWrite(relai, HIGH);}
+  if(runfan1 == true){ 
+                      digitalWrite(relai, LOW);
+                      digitalWrite(onboardled, LOW);}
+  if(runfan1 == false){
+                        digitalWrite(relai, HIGH); 
+                        digitalWrite(onboardled, HIGH);}
   }
